06b08e7729159cf792b0f49fa66cf55c1aaffa76
Jakub Dvořák <567772@mail.muni.cz>
2025-11-28 08:48:17 +0100

    fix: Fix linting issues in the codebase Because nodejs 16 does not support lint, but uses eslint instead, i have updated some dependencies and fixed the linting issues in the codebase.
    now for format and linting, commands `pnpm lint:fix` and `pnpm format`
    can be used.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { type PropsWithChildren } from 'react';
 import { Toaster } from 'sonner';
--- src/repositories/example/example-repository.ts
+++ src/repositories/example/example-repository.ts
@@ -3,7 +3,8 @@ import type { ExampleCreateEntity } from './schema';
 export const exampleRepository = {
 	async createExample(exampleEntity: ExampleCreateEntity) {
 		console.log(
-			"There should be called functions like: 'await db.user.create()'"
+			"There should be called functions like: 'await db.user.create()'",
+			exampleEntity
 		);
 		return null; // should return created object
 	}
