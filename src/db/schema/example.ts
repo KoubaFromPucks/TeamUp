24717cace95305ab8a813cb00f2128c00297f3fe
Jakub Dvořák <567772@mail.muni.cz>
2025-11-29 20:48:54 +0100

    fix: Remove examples

--- src/db/schema/example.ts
+++
@@ -1,9 +0,0 @@
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
-
-export const exampleTable = sqliteTable('users', {
-	id: text('id').primaryKey(),
-	name: text('name').notNull()
-});
-
-// Typ generovaný Drizzle
-export type DrizzleExample = typeof exampleTable.$inferSelect;
