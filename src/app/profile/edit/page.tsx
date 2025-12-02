8aca5b74521f306c99f9cf43a07021b6a2b4cd21
Jakub Dvořák <567772@mail.muni.cz>
2025-12-02 10:55:43 +0100

    feat: Add arrow to navigate back

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { getUserById } from '@/facades/user/user-facade';
 import { UpdateUserForm } from '@/modules/user/components/update-user-form';
+import { ChevronLeft } from 'lucide-react';
 
 const Page = async () => {
 	const { error, user } = await getUserById('user_1_uuid'); // TODO: replace with actual user ID
+	const returnPath = `/profile/${user?.id}`;
 
 	if (!user || error) {
 		return (
@@ -15,8 +17,13 @@ const Page = async () => {
 
 	return (
 		<>
-			<h1 className="mb-6 text-3xl font-semibold">User Profile</h1>
-			<UpdateUserForm user={{ ...user }} navPath={`/profile/${user.id}`} />
+			<div className="flex items-center mb-6">
+				<a href={returnPath}>
+					<ChevronLeft className="h-7 w-7 cursor-pointer" />
+				</a>
+				<h1 className="text-3xl font-semibold">Edit User Profile</h1>
+			</div>
+			<UpdateUserForm user={{ ...user }} navPath={returnPath} />
 		</>
 	);
 };
