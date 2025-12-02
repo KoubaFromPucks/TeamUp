a561e1dd42c3b21d6f3584abc464a40743989c14
Jakub Dvořák <567772@mail.muni.cz>
2025-12-02 16:09:10 +0100

    fix: Remove redundant line

--- src/db/schema/team-member.ts
+++ src/db/schema/team-member.ts
@@ -16,5 +16,3 @@ export const teamMemberTable = sqliteTable(
 	},
 	table => [primaryKey({ columns: [table.userId, table.teamId] })]
 );
-
-export type DrizzleTeamMember = typeof teamMemberTable.$inferSelect;
