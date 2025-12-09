bf6fe6e2f0db228e772ae6d24b7057eef06f3329
Jakub Dvořák <567772@mail.muni.cz>
2025-12-09 12:53:25 +0100

    fix: Remove unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import { ConcreteEventsList } from '@/modules/concreteEvent/components/concrete-events-list';
 import { EventsListSkeleton } from '@/modules/event/components/skeletons/events-list-skeleton';
 import React, { Suspense } from 'react';
-import { eventService } from '@/services/event/service';
 
 const Home = async () => {
-	await eventService.getPriceForPayLaterOptionSelectedForEachConcreteEvent(
-		'9d15b9ea-061c-4747-9215-b1d2c93b693f'
-	);
-
 	return (
 		<Suspense fallback={<EventsListSkeleton></EventsListSkeleton>}>
 			<ConcreteEventsList></ConcreteEventsList>
